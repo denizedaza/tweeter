0bb8ce45b2ca3de3a59bebe27a462e971087a4db
denizedaza <dazadenize@gmail.com>
2021-09-09 16:21:08 -0400

    remove unused comment code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 
   $('#tweet-text').on('input', function () {
-    // console.log("this:", this); // 'this' refers to the textarea html
-    // <textarea name="text" id="tweet-text"></textarea>
     const maxValue = 140;
     const charsTypedLength = $(this).val().length;
     const charsRemaining = maxValue - charsTypedLength;
@@ -15,10 +13,6 @@ $(document).ready(function() {
       counter.removeClass('invalid');
     }
 
-
-    // console.log("counter var:", counter);
-
-    // console.log("this length:", charsTypedLength);
   })
 
 });
